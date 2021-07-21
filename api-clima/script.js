7c44c20858e98ba0b7997e5b28505a5fcb1b19ab
Banzak-Uninter <leosantanabr@gmail.com>
2021-07-21 15:23:42 -0300

    app de requisição de clima

---
+++ api-clima/script.js
@@ -0,0 +1,59 @@
+document.querySelector('.busca').addEventListener('submit', async (event)=> {
+    event.preventDefault();
+
+    let input = document.querySelector('#searchInput').nodeValue;
+
+    if(input !== '') {
+        clearInfo();
+
+        showWarning('Carregando...');
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=efcef971b8ece62b4b68f41a58e74b42&units=metric&lang=pt_br`
+        
+        let result = await fetch(url);
+
+        let json = await result.json();
+    
+        if(json.cod === 200) {
+            
+            sohwInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg,
+            });
+        } else {
+            clearInfo();
+
+            showWarning('Não encontramos essa localização.');
+            
+        }
+    } else {
+        clearInfo();
+    }
+});
+
+
+function sohwInfo(json) {
+    showWarning('');
+
+    document.querySelector('.resultado').style.display = 'block';
+
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector('.tempInfo').innerHTML = `${Math.round(json.temp)} <sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${Math.round(json.windSpeed)} <span>km/h</span>`;
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle - 90}deg);`
+}
+
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML = msg;
+}
+
+function clearInfo() {
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none';
+
+}
